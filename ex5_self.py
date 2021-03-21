0c58a385b982ac2388df01c6d7d78460d26ca129
GustavoGomes9 <gustavo.isaque9@gmail.com>
2021-03-20 21:41:17 -0300

    Formats

--- ex5_self.py
+++ ex5_self.py
@@ -1,6 +1,6 @@
 def calc_gas(cash_value, gas_value):
-    result = cash_value // gas_value
-    return result
+    result = cash_value / gas_value
+    return f'Voce terá {result:.2f}L de gasolina' #Formatted string literals
 
 arg1 = float(input('tell me how much are you buying: '))
 arg2 = float(input('And how much the gas cost: '))
---
+++ ex8.py
@@ -0,0 +1,6 @@
+hero = ['Batman', 'Superman', 'Aquaman']
+aka = ['Bruce', 'Clark', 'Arthur']
+
+for i, v in zip(hero, aka):
+    print('{0:5} is: {1}'.format(i, v)) #you choose your format of output
+
