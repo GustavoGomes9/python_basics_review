ff9b595d8dad16d2d421cc5aa9b05f24235eb38b
GustavoGomes9 <gustavo.isaque9@gmail.com>
2021-03-21 19:40:48 -0300

    Errors and exceptions

--- ex8.py
+++ ex8.py
@@ -1,6 +1,33 @@
+
 hero = ['Batman', 'Superman', 'Aquaman']
 aka = ['Bruce', 'Clark', 'Arthur']
 
 for i, v in zip(hero, aka):
     print('{0:5} is: {1}'.format(i, v)) #you choose your format of output
+
+# Handling Exceptions
+try:
+    x = 1 + 1 #try put '' to test
+except:
+    x = 5 + 5
+else: 
+    print('--------else----------')
+finally:
+    print(f'The number is: {x}')
+
+# Raising exceptions
+try: 
+    raise NameError('test')
+except NameError:
+    print('Exception flew by')
+    raise
+ 
+# Exception Chaining - cadeia de exceções
+
+def foo():
+    raise IOError
+try:
+    foo()
+except IOError as exc:
+    raise RuntimeError('Failed to open database') from exc
 
